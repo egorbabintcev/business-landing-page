e3fc9c565fea21e9b1c8fa82d7301e8791881fe8
Egor Babintsev <e.e.babintsev@yandex.ru>
2020-04-09 10:49:46 +0300

    Added function for closing menu if clicked outside

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,14 @@
 const toggler = document.querySelector('.nav-menu__toggler')
 const menu = document.querySelector(toggler.dataset.target);
 
-// TODO menu close if clicked outside
 toggler.addEventListener('click', (evt) => {
   menu.classList.toggle('expanded');
 });
+
+menu.parentNode.addEventListener('click', (evt) => {
+  evt.stopPropagation();
+})
+
+document.addEventListener('click', (evt) => {
+  menu.classList.remove('expanded');
+})
